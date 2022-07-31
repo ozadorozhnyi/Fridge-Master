1b47c6d44ff4dbdc8a568d53cc8cbd99050818f7
Oleg Zadorozhnyi <ipfound@gmail.com>
2022-07-31 12:42:54 +0300

    domain area tables migrations added.

---
+++ database/migrations/2022_07_31_083830_create_domain_area_tables.php
@@ -0,0 +1,131 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDomainAreaTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('timezone');
+            $table->timestamps();
+        });
+
+        Schema::create('locations_buildings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('location_id');
+            $table->integer('temperature')->default(0);
+            $table->timestamps();
+
+            $table->foreign('location_id')
+                ->references('id')
+                ->on('locations')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+
+        Schema::create('locations_buildings_blocks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('building_id');
+            $table->unsignedFloat('current_rent_price')->default(0);
+            $table->timestamps();
+            $table->foreign('building_id')
+                ->references('id')
+                ->on('locations_buildings')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('contract_id')->after('id');
+
+            $table->foreign('contract_id')
+                ->references('id')
+                ->on('contracts')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('location_id');
+            $table->char('access_code', 12);
+            $table->unsignedInteger('requested_volume');
+            $table->smallInteger('requested_temperature');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('status', ['reserved', 'progress', 'archived']);
+            $table->unsignedFloat('total_costs_on_book_date');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->foreign('location_id')
+                ->references('id')
+                ->on('locations')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+
+        Schema::create('busy_blocks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('block_id');
+            $table->foreignId('booking_id');
+            $table->enum('status', ['reserved', 'busy']);
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->float('rent_price_on_booking_date');
+            $table->timestamps();
+
+            $table->foreign('block_id')
+                ->references('id')
+                ->on('locations_buildings_blocks')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->foreign('booking_id')
+                ->references('id')
+                ->on('bookings')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('busy_blocks');
+        Schema::dropIfExists('bookings');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_contract_id_foreign');
+            $table->dropColumn('contract_id');
+        });
+        Schema::dropIfExists('contracts');
+        Schema::dropIfExists('locations_buildings_blocks');
+        Schema::dropIfExists('locations_buildings');
+        Schema::dropIfExists('locations');
+    }
+}
